6adfd1374660b667660f9583642968fea9ec7d6e
Younghoon CHOI _^ <WachsenHaus@gmail.com>
2021-03-16 17:41:00 +0900

    <추가> : <재미삼아 재귀함수 구구단>

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
 console.log("hello world~");
+
+function recurGUGU(dan,depth){
+    if(depth===1) {
+        return dan*depth;
+    }
+    const result = dan+recurGUGU(dan,depth-1)
+    console.log(`${dan} * ${depth} = ${result}`);
+    return result;
+}
+
+recurGUGU(9,9);
