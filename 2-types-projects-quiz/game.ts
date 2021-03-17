354971588dafda71a2b3d4ab42cdcd79c1bb4c00
Younghoon CHOI _^ <WachsenHaus@gmail.com>
2021-03-17 15:50:58 +0900

    <추가>: <function loading 퀴즈 품>

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,36 @@
 /**
  * Let's make a game 🕹
  */
+{
+type Position = {
+    x: number;
+    y: number;
+}
+
+const position:Position ={
+    x:0,
+    y:0
+}
+type Mode = "up" | "down" | "left" | "right";
+function move(mode:Mode){
+    switch(mode)
+    {
+        case "up":
+            position.y++;
+            break;
+        case "down":
+            position.y--;
+            break;
+        case "left":
+            position.x--;
+            break;
+        case "right":
+            position.x++;
+            break;
+        default:
+            throw Error("move에러!")
+    }
+}
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
@@ -10,3 +40,5 @@ move('left');
 console.log(position); // { x: -1, y: 0}
 move('right');
 console.log(position); // { x: 0, y: 0}
+
+}
--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -23,4 +23,16 @@
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+
+  function printLoginState( state:ResourceLoadState){
+    if( state.state === "fail"){
+      console.log(`😱 no network`);
+    }else if(state.state === "loading"){
+      console.log(`👀 loading...`);
+    }else if(state.state === "success"){
+      console.log(`😃 ${state.response.body}`);
+    }else{
+      throw new Error(`상태에러${state}`)
+    }
+  }
 }
