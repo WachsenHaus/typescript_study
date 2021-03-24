45c3331e1de2e69d3f1b40f50735eb3f7e398abf
Wachsenhaus <bi9choi@gmail.com>
2021-03-24 15:33:52 +0900

    <추가> : <제네릭 클래스>

---
+++ 5-generic/5-1-function.ts
@@ -0,0 +1,28 @@
+{
+    // function checkNotNull(arg: number | null): number{
+    //     if(arg == null){
+    //         throw new Error("not valid number!")
+    //     }
+    //     return arg;
+    // }
+
+    function checkNotNullAnyBad(arg:any | null) : any{
+        if(arg == null)
+        {
+            throw new Error("not valid number!")
+        }
+        return arg;
+    }
+
+    function checkNotNull<T>(arg: T | null) : T {
+        if(arg == null){
+            throw new Error("null 오류!!!")
+        }
+        return arg;
+    }
+
+    const number = checkNotNull(123);
+
+    const boal:boolean = checkNotNull(true)
+    
+}
