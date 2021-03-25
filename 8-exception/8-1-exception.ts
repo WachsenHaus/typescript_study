75ed040a102ec15364d7d897f73460364becd0fa
Wachsenhaus <bi9choi@gmail.com>
2021-03-25 13:38:06 +0900

    <추가> : <익셉션 연습>' ;

--- 8-exception/8-1-exception.ts
+++ 8-exception/8-1-exception.ts
@@ -1,11 +1,33 @@
-{
-    //java : exception
-    // javascript : error
+// {
+//     //java : exception
+//     // javascript : error
 
 
-    const array = new Array(10000000000000000);
+//     const array = new Array(10000000000000000);
 
 
 
-    
-}
+    function readFile(fileName: string) : string{
+        if(fileName === "not Exist! 똥이다!"){
+            throw new Error(`파일이 존재하지 않아! ${fileName}`)
+        }
+
+        return fileName;
+    }
+
+    function closeFile(fileName: string){
+        //
+    }
+
+    const fileName = 'not Exist! 똥이다!';
+
+    //에러가 나는곳만 try catch를 하는것이 좋다.
+    try{
+        console.log(readFile("A"));
+    }catch(error){
+        console.log('catched!!');
+    }finally{
+        //캐치안에서 어떠한 일이 생길지 모르기 때문에 finally에 넣는것이 좋다.
+        closeFile(fileName);
+        console.log("힝구")
+    }
